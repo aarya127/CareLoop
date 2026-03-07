2b739e8523b4e0669e773820495387932e0e9b14
aarya127 <353321854@educ.dpcdsb.org>
2026-03-07 18:13:50 -0500

    empty

--- Patient Management/Data/kaggle.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("humansintheloop/teeth-segmentation-on-dental-x-ray-images")
-
-print("Path to dataset files:", path)
-
