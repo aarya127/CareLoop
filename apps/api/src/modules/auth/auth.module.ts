5cf6adc03bde1f1d7da878bd812754b9438a0dc1
aarya127 <353321854@educ.dpcdsb.org>
2026-05-14 00:17:32 -0400

    fix: make AuthModule global so guards are available in all feature modules

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PasswordService } from './password.service';
 import { SessionService } from './session.service';
 import { AuthGuard, RolesGuard, ServiceAccountGuard } from '../../common/guards';
 
+@Global()
 @Module({
   controllers: [AuthController],
   providers: [AuthService, PasswordService, SessionService, AuthGuard, RolesGuard, ServiceAccountGuard],
